75a12549f03381ae7ea6ae0c5d46aa7ce4eeb011
Xingyang Li <brucelee_sjtu@sjtu.edu.cn>
2025-07-04 05:45:57 +0800

    feat: Support FusionX (#13)
    * feat: integrate FusionX LoRA inference for Wan2.1-14B
    
    * docs: add FusionX description
    
    ---------
    
    Co-authored-by: Xingyang Li <xyli05@mit.edu>

--- wan_t2v_inference.py
+++ wan_t2v_inference.py
@@ -22,7 +22,10 @@ if __name__ == "__main__":
     parser.add_argument("--num_inference_steps", type=int, default=50, help="Number of denoising steps in the generated video")
     parser.add_argument("--output_file", type=str, default="output.mp4", help="Output video file name")
     parser.add_argument("--seed", type=int, default=0, help="Random seed for generation")
-
+    parser.add_argument("--guidance_scale", type=float, default=5.0, help="Guidance scale for classifier-free guidance")
+    parser.add_argument("--flow_shift", type=float, default=5.0, help="Flow shift for the scheduler, adjust based on video resolution (5.0 for 720P, 3.0 for 480P)")
+    parser.add_argument("--lora_checkpoint_dir", type=str, default=None, help="Directory containing LoRA checkpoint files")
+    parser.add_argument("--lora_checkpoint_name", type=str, default=None, help="Name of the LoRA checkpoint file to load, if not specified, will use the default name in the directory")
     parser.add_argument("--pattern", type=str, default="dense", choices=["radial", "dense"])
     parser.add_argument("--dense_layers", type=int, default=0, help="Number of dense layers to use in the Wan attention, set to 1 for 1x length video, 2 for 2x length video")
     parser.add_argument("--dense_timesteps", type=int, default=0, help="Number of dense timesteps to use in the Wan attention, set to 12 for 1x length video, 2 for 2x length video")
@@ -34,10 +37,15 @@ if __name__ == "__main__":
     # Available models: Wan-AI/Wan2.1-T2V-14B-Diffusers, Wan-AI/Wan2.1-T2V-1.3B-Diffusers
     model_id = "Wan-AI/Wan2.1-T2V-14B-Diffusers"
     vae = AutoencoderKLWan.from_pretrained(model_id, subfolder="vae", torch_dtype=torch.float32)
-    flow_shift = 5.0 # 5.0 for 720P, 3.0 for 480P
+    flow_shift = args.flow_shift
     scheduler = UniPCMultistepScheduler(prediction_type='flow_prediction', use_flow_sigmas=True, num_train_timesteps=1000, flow_shift=flow_shift)
     pipe = WanPipeline.from_pretrained(model_id, vae=vae, torch_dtype=torch.bfloat16)
     pipe.scheduler = scheduler
+    if args.lora_checkpoint_dir is not None:
+        pipe.load_lora_weights(
+            args.lora_checkpoint_dir,
+            weight_name=args.lora_checkpoint_name,
+        )
     pipe.to("cuda")
 
     if args.prompt is None:
@@ -68,7 +76,7 @@ if __name__ == "__main__":
         height=args.height,
         width=args.width,
         num_frames=args.num_frames,
-        guidance_scale=5.0,
+        guidance_scale=args.guidance_scale,
         num_inference_steps=args.num_inference_steps
     ).frames[0]
     
