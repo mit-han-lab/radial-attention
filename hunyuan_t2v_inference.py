fix: merge pre-defined attention mask with radial attention mask during flashinfer

hunyuan_t2v_inference.py
@@ -5,6 +5,7 @@ import torch
 from diffusers import HunyuanVideoPipeline, HunyuanVideoTransformer3DModel
 from diffusers.utils import export_to_video
 import argparse
+from diffusers.schedulers.scheduling_flow_match_euler_discrete import FlowMatchEulerDiscreteScheduler
 
 from radial_attn.utils import set_seed
 from radial_attn.models.hunyuan.inference import replace_hunyuan_attention
@@ -55,13 +56,12 @@ if __name__ == "__main__":
         torch_dtype=torch.bfloat16
     )
     pipe = HunyuanVideoPipeline.from_pretrained(
-        args.model_id, 
-        transformer=transformer, 
-        torch_dtype=torch.float16
+        args.model_id,  
+        transformer=transformer,
+        torch_dtype=torch.bfloat16
     )
     pipe.vae.enable_tiling()
     pipe.to("cuda")
-    # pipe.scheduler._shift = 7
     if args.lora_checkpoint_dir:
         print(f"Loading LoRA weights from {args.lora_checkpoint_dir}")
         config_path = os.path.join(args.lora_checkpoint_dir, "lora_config.json")

radial_attn/attn_mask.py
@@ -147,14 +147,24 @@ class MaskMap:
         log_mask[:block_bound, :block_bound] = self.log_mask[:block_bound, :block_bound]
         return log_mask
 
-def RadialAttention(query, key, value, mask_map=None, sparsity_type="radial", block_size=128, decay_factor=1, model_type=None):
+def RadialAttention(query, key, value, mask_map=None, sparsity_type="radial", block_size=128, decay_factor=1, model_type=None, pre_defined_mask=None):
     orig_seqlen, num_head, hidden_dim = query.shape
     
     query = pad_qkv(query, block_size=block_size)
     key = pad_qkv(key, block_size=block_size)
     value = pad_qkv(value, block_size=block_size)
-    
-    mask = mask_map.queryLogMask(query, sparsity_type, block_size=block_size, decay_factor=decay_factor, model_type=model_type) if mask_map else None
+    if sparsity_type == "dense":
+        mask = torch.ones((query.shape[0] // block_size, query.shape[0] // block_size), device=query.device, dtype=torch.bool)
+    else:
+        mask = mask_map.queryLogMask(query, sparsity_type, block_size=block_size, decay_factor=decay_factor, model_type=model_type) if mask_map else None
+    if pre_defined_mask is not None:
+        # shrink the pre-defined mask to the block size
+        extended_mask = torch.zeros((query.shape[0] // block_size, query.shape[0] // block_size), device=query.device, dtype=torch.bool)
+        extended_mask[:pre_defined_mask.shape[0] // block_size, :pre_defined_mask.shape[1] // block_size] = pre_defined_mask.reshape(
+            pre_defined_mask.shape[0] // block_size, block_size, pre_defined_mask.shape[1] // block_size, block_size
+        ).any(dim=(1, 3))
+        extended_mask[pre_defined_mask.shape[0] // block_size:, :] = True
+        mask = torch.logical_and(mask, extended_mask)
     seqlen = query.shape[0]
     workspace_buffer = torch.empty(128 * 1024 * 1024, device=query.device, dtype=torch.uint8)
     bsr_wrapper = flashinfer.BlockSparseAttentionWrapper(

radial_attn/models/hunyuan/attention.py
@@ -93,27 +93,28 @@ class HunyuanVideoAttnSparseProcessor2_0:
 
         # 5. Attention
         if timestep is None: # this is the case for dense attention
-            with sdpa_kernel(bzsackends=[SDPBackend.FLASH_ATTENTION]):
-                hidden_states = F.scaled_dot_product_attention(
-                    query, key, value, dropout_p=0.0, is_causal=False
-                )
+            hidden_states = F.scaled_dot_product_attention(
+                query, key, value, attn_mask = attention_mask, dropout_p=0.0, is_causal=False
+            )
         else:  # this is the case for sparse attention
             # print(f"numeral_timestep: {numeral_timestep}, dense_timestep: {self.dense_timestep}, layer_idx: {self.layer_idx}, dense_block: {self.dense_block}, sparse_type: {self.sparse_type}")
+            pre_defined_mask=attention_mask[0, 0].expand(query.shape[0], query.shape[0])
+            batch_size = query.shape[0]
+            query = rearrange(query, "b h s d" " -> (b s) h d")
+            key = rearrange(key, "b h s d" " -> (b s) h d")
             if numeral_timestep < self.dense_timestep or self.layer_idx < self.dense_block or self.sparse_type == "dense":
-                with sdpa_kernel(backends=[SDPBackend.FLASH_ATTENTION]):
-                    hidden_states = F.scaled_dot_product_attention(
-                        query, key, value, dropout_p=0.0, is_causal=False
-                    )
-            else:
-                batch_size = query.shape[0]
-                query = rearrange(query, "b h s d" " -> (b s) h d")
-                key = rearrange(key, "b h s d" " -> (b s) h d")
-                value = rearrange(value, "b h s d" " -> (b s) h d")
                 # apply radial attention
                 hidden_states = RadialAttention(
-                    query=query, key=key, value=value, mask_map=self.mask_map, sparsity_type=self.sparse_type, block_size=128, decay_factor=self.decay_factor, model_type="hunyuan",
+                    query=query, key=key, value=value, mask_map=self.mask_map, sparsity_type="dense", block_size=128, decay_factor=self.decay_factor, model_type="hunyuan", pre_defined_mask=pre_defined_mask
                 )
-                hidden_states = rearrange(hidden_states, "(b s) h d -> b h s d", b=batch_size)
+                
+            else:
+                # apply radial attention
+                hidden_states = RadialAttention(
+                    query=query, key=key, value=value, mask_map=self.mask_map, sparsity_type=self.sparse_type, block_size=128, decay_factor=self.decay_factor, model_type="hunyuan", pre_defined_mask=pre_defined_mask
+                )
+                
+            hidden_states = rearrange(hidden_states, "(b s) h d -> b h s d", b=batch_size)
         hidden_states = hidden_states.transpose(1, 2).flatten(2, 3)
         hidden_states = hidden_states.to(query.dtype)
 